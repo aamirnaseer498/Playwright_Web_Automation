8bac32b4a0007a6c9ae5131cac518da163aa6af0
Aamir Naseer <aamirnaseer498@gmail.com>
2025-11-21 17:19:59 +0500

    Commited Locators

--- tests/Locators.spec.js
+++ tests/Locators.spec.js
@@ -1,7 +1,10 @@
 
 const {test, expect} = require ("@playwright/test")
 
-test ("Locators", async ({page})=>{
+
+// Example 1
+
+test ("Locators1", async ({page})=>{
 
 await page.goto("https://www.demoblaze.com/index.html")
 
@@ -15,6 +18,49 @@ await page.locator("//button[normalize-space()='Log in']").click()
 
 let logOutButton= await page.locator("//a[@id='logout2']")
 await expect(logOutButton).toBeVisible()
+
+await page.close()
+
+})
+
+
+
+// Example 2
+
+test ("Locators2", async ({page})=>{
+
+await page.goto("https://www.demoblaze.com/index.html")
+
+let links= await page.$$("//a")
+
+for (let link of links){
+
+    console.log(await link.textContent())
+
+}
+
+await page.close()
+
+})
+
+
+
+// Example 3
+
+test ("Locators3", async ({page})=>{
+
+await page.goto("https://www.demoblaze.com/index.html")
+
+await page.waitForSelector("//div[@id='tbodyid']//div//h4//a")
+
+let products= await page.$$("//div[@id='tbodyid']//div//h4//a")
+
+for (let product of products){
+
+    let productName= await product.textContent()
+    console.log(productName)
+
+}
 
 await page.close()
 
