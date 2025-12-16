9c48953e7a3137bf9be5b27518ed8708a9c5d375
Amir <aamirnaseer498@gmail.com>
2025-12-16 20:02:45 +0500

    Commited Drop Down

--- tests/DropDown.spec.js
+++ tests/DropDown.spec.js
@@ -36,7 +36,33 @@ test("Drop Down 4", async ({page})=>{
 
     await page.locator("#country").selectOption({index: 6})
 
-    await page.waitForTimeout(3000)
+    await page.close()
+
+})
+
+test("Drop Down Assertion 1", async ({page})=>{
+
+    await page.goto("https://testautomationpractice.blogspot.com/")
+
+    const options= await page.locator("#country option")
+
+    console.log("Number of options: " + await options.count())
+
+    await expect(options).toHaveCount(10)
+
+    await page.close()
+
+})
+
+test("Drop Down Assertion 2", async ({page})=>{
+
+    await page.goto("https://testautomationpractice.blogspot.com/")
+
+    const options= await page.locator("#country option")
+
+    await console.log((await options.allTextContents()).toString())
+
+    await expect(((await options.allTextContents()).toString().includes("China"))).toBeTruthy()
 
     await page.close()
 
