ccd4ffe3b76ed26096608c17644b9c21762d2799
Amir <aamirnaseer498@gmail.com>
2025-12-19 18:03:45 +0500

    Commited Drop Down

--- tests/DropDown.spec.js
+++ tests/DropDown.spec.js
@@ -1,69 +1,119 @@
 const {test, expect}= require("@playwright/test")
 
-test("Drop Down 1", async ({page})=>{
+// test("Drop Down 1", async ({page})=>{
+
+//     await page.goto("https://testautomationpractice.blogspot.com/")
+
+//     await page.locator("#country").selectOption({label: "Germany"})
+
+//     await page.close()
+
+// })
+
+// test("Drop Down 2", async ({page})=>{
+
+//     await page.goto("https://testautomationpractice.blogspot.com/")
+
+//     await page.locator("#country").selectOption("Germany")
+
+//     await page.close()
+
+// })
+
+// test("Drop Down 3", async ({page})=>{
+
+//     await page.goto("https://testautomationpractice.blogspot.com/")
+
+//     await page.locator("#country").selectOption({value: "uk"})
+
+//     await page.close()
+
+// })
+
+// test("Drop Down 4", async ({page})=>{
+
+//     await page.goto("https://testautomationpractice.blogspot.com/")
+
+//     await page.locator("#country").selectOption({index: 6})
+
+//     await page.close()
+
+// })
+
+test("Drop Down 5", async ({page})=>{
 
     await page.goto("https://testautomationpractice.blogspot.com/")
 
-    await page.locator("#country").selectOption({label: "Germany"})
+    const options= await page.$$("#country option")
+
+    for(const option of options){
+
+        let value= await option.textContent()
+        value= value.trim()
+    
+        console.log(value)
+
+        if(value.includes("Australia")){
+            await page.selectOption("#country", value)
+            break
+        }
+    
+    }
 
     await page.close()
 
 })
 
-test("Drop Down 2", async ({page})=>{
+// test("Drop Down Assertion 1", async ({page})=>{
 
-    await page.goto("https://testautomationpractice.blogspot.com/")
+//     await page.goto("https://testautomationpractice.blogspot.com/")
 
-    await page.locator("#country").selectOption("Germany")
+//     const options= await page.locator("#country option")
 
-    await page.close()
+//     console.log("Number of options: " + await options.count())
 
-})
+//     await expect(options).toHaveCount(10)
 
-test("Drop Down 3", async ({page})=>{
+//     await page.close()
 
-    await page.goto("https://testautomationpractice.blogspot.com/")
+// })
 
-    await page.locator("#country").selectOption({value: "uk"})
+// test("Drop Down Assertion 2", async ({page})=>{
 
-    await page.close()
+//     await page.goto("https://testautomationpractice.blogspot.com/")
 
-})
+//     const options= await page.locator("#country option")
 
-test("Drop Down 4", async ({page})=>{
+//     await console.log((await options.allTextContents()).toString())
 
-    await page.goto("https://testautomationpractice.blogspot.com/")
+//     await expect(((await options.allTextContents()).toString().includes("China"))).toBeTruthy()
 
-    await page.locator("#country").selectOption({index: 6})
+//     await page.close()
 
-    await page.close()
+// })
 
-})
+// test("Drop Down Assertion 3", async ({page})=>{
 
-test("Drop Down Assertion 1", async ({page})=>{
+//     await page.goto("https://testautomationpractice.blogspot.com/")
 
-    await page.goto("https://testautomationpractice.blogspot.com/")
+//     let status= false
 
-    const options= await page.locator("#country option")
+//     const options= await page.$$("#country option")
 
-    console.log("Number of options: " + await options.count())
+//     for(const option of options){
 
-    await expect(options).toHaveCount(10)
+//         let value= await option.textContent()
+//         console.log(value)
+        
+//         if(value.includes("Canada")){
+//             status= true
+//             break
+//         }
 
-    await page.close()
+//     }
 
-})
+//     await expect(status).toBeTruthy()
 
-test("Drop Down Assertion 2", async ({page})=>{
+//     await page.close()
 
-    await page.goto("https://testautomationpractice.blogspot.com/")
-
-    const options= await page.locator("#country option")
-
-    await console.log((await options.allTextContents()).toString())
-
-    await expect(((await options.allTextContents()).toString().includes("China"))).toBeTruthy()
-
-    await page.close()
-
-})
+// })
