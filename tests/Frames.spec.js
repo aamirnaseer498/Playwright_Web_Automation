d5747d1c48cbc3407dadfdcfd04d81b36c3a60bd
Amir <aamirnaseer498@gmail.com>
2026-01-07 14:50:01 +0500

    Commited Frames

--- tests/Frames.spec.js
+++ tests/Frames.spec.js
@@ -41,3 +41,34 @@ test("Frames using Frame Locator", async ({page})=>{
     await page.close()
 
 })
+
+test("Nested Frames", async ({page})=>{
+
+    await page.goto("https://ui.vision/demo/webtest/frames/")
+
+    const frames= await page.frames()
+
+    console.log("Total Available Frames:", frames.length)
+
+    for(let frame of frames){
+        console.log("Frame Name:", await frame.title())
+        console.log("Frame URL:", await frame.url())
+    }
+
+    const frame= await page.frame({url: "https://ui.vision/demo/webtest/frames/frame_3.html"})
+
+    await frame.locator("input[name='mytext3']").fill("Playwright")
+
+    const childFrames= frame.childFrames()
+
+    console.log("Total Child Frames:", childFrames.length)
+    console.log("Child Frame Name:", await childFrames[0].title())
+    console.log("Child Frame URL:", await childFrames[0].url())
+
+    await childFrames[0].locator("//*[@id='i9']/div[3]/div").check()
+
+    await page.waitForTimeout(3000)
+
+    await page.close()
+
+})
