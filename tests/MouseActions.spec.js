75c24032f96bcb175f64fab32e1fbe3cee9dda30
Aamir Naseer <aamirnaseer498@gmail.com>
2026-05-11 18:57:41 +0500

    Commited Mouse Actions

--- tests/MouseActions.spec.js
+++ tests/MouseActions.spec.js
@@ -1,4 +1,5 @@
 const {test, expect}= require("@playwright/test")
+const { it } = require("node:test")
 
 test("Mouse Hover Action", async ({page})=>{
 
@@ -40,6 +41,36 @@ test("Mouse Double Click Action", async ({page})=>{
     const inputField= await page.locator("//input[@id='field2']")
 
     await expect(inputField).toHaveValue("Hello World!")
+
+    await page.waitForTimeout(5000)
+
+})
+
+test("Mouse Drag and Drop Action", async ({page})=>{
+
+    await page.goto("https://www.testautomationstudio.com/demo/actions/")
+
+    const item1= await page.locator("ul[id='sortable1'] li:nth-child(1)")
+
+    const item2= await page.locator("ul[id='sortable1'] li:nth-child(5)")
+
+    const container= await page.locator("#sortable2")
+
+    // Approach 1
+
+    await item1.hover()
+
+    await page.mouse.down()
+
+    await container.hover()
+
+    await page.waitForTimeout(2000)
+
+    await page.mouse.up()
+
+    // Approach 2
+
+    await item2.dragTo(container)
 
     await page.waitForTimeout(5000)
 
