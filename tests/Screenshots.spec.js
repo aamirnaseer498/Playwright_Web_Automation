f31a67550ee96021e3a1fd8aadf99b5d36365760
Aamir Naseer <aamirnaseer498@gmail.com>
2026-06-29 19:50:33 +0500

    Commited Screenshots

--- tests/Screenshots.spec.js
+++ tests/Screenshots.spec.js
@@ -6,3 +6,19 @@ test("Page Screenshot", async ({page})=>{
     await page.screenshot({path: "tests/Screenshots/" + Date.now() + "HomePage.png"})
 
 })
+
+test("Full Page Screenshot", async ({page})=>{
+
+    await page.goto("https://demoblaze.com/index.html")
+    await page.waitForTimeout(5000)
+    await page.screenshot({path: "tests/Screenshots/" + Date.now() + "FullPage.png", fullPage: true})
+
+})
+
+test("Element Screenshot", async ({page})=>{
+
+    await page.goto("https://demoblaze.com/index.html")
+    await page.waitForTimeout(5000)
+    await page.locator("//body/div[@id='contcont']/div[@class='row']/div[@class='col-lg-9']/div[@id='tbodyid']/div[2]/div[1]").screenshot({path: "tests/Screenshots/" + Date.now() + "Mobile.png"})
+
+})
