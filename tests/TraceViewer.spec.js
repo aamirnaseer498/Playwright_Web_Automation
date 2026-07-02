2a80faecdb74ac7e828c40e5080feeee94e0d389
Aamir Naseer <aamirnaseer498@gmail.com>
2026-07-02 19:52:14 +0500

    Commited Trace Viewer

---
+++ tests/TraceViewer.spec.js
@@ -0,0 +1,19 @@
+const {test, expect}= require("@playwright/test")
+
+
+test("Trace Viewer", async ({page})=>{
+
+await page.goto("https://testautomationpractice.blogspot.com/")
+
+await expect(await page.locator("#name")).toBeVisible()
+await expect(await page.locator("#name")).toBeEnabled()
+await expect(await page.locator("#name")).toBeEditable()
+await expect(await page.locator("#name")).toBeEmpty()
+
+await page.locator("#name").fill("Aamir")
+
+await page.waitForTimeout(2500)
+
+await page.close()
+
+})
