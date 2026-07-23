03e3000f57e10c1095ce54d856c9d6c65e3b26f8
Aamir Naseer <aamir.naseer@imaginationai.net>
2026-07-23 20:00:24 +0500

    Commited Annotations

---
+++ tests/Annotations.spec.js
@@ -0,0 +1,37 @@
+import {test, expect} from "@playwright/test"
+
+test("Test 1", async ({page})=>{
+
+    console.log("This is test 1")
+
+})
+
+test("Test 2", async ({page})=>{
+
+    console.log("This is test 2")
+
+})
+
+test("Test 3", async ({page})=>{
+
+    console.log("This is test 3")
+
+})
+
+test("Test 4", async ({page})=>{
+
+    test.fixme()
+
+    console.log("This is test 4")
+
+})
+
+test("Test 5", async ({page, browserName})=>{
+
+    console.log("This is test 5")
+
+    if(browserName === "firefox"){
+        test.fail()
+    }
+
+})
