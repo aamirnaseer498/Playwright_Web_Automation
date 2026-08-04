d1ff13f1114b8894e74f4f24b0fcf579253e2f65
Aamir Naseer <aamir.naseer@imaginationai.net>
2026-08-04 19:36:19 +0500

    Commited Retry Failed Tests

--- tests/Reporters.spec.js
+++ tests/Reporters.spec.js
@@ -3,13 +3,15 @@ import {test, expect} from "@playwright/test"
 test("Test 1", async ({page})=>{
 
     await page.goto("https://www.demoblaze.com/index.html")
-    expect(page).toHaveTitle("STORE")
+    await page.waitForTimeout(3000)
+    await expect(page).toHaveTitle("STORE")
 
 })
 
 test("Test 2", async ({page})=>{
 
     await page.goto("https://testautomationpractice.blogspot.com/")
-    expect(page).toHaveTitle("Automation Testing Practice")
+    await page.waitForTimeout(3000)
+    await expect(page).toHaveTitle("Automation Testing Practice")
 
 })
